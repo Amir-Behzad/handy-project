c0e2c05f8e21990a258d011962488d9b3f353a06
Amir Behzad <a.behzad.ebr@gmail.com>
2019-10-17 12:18:47 +0330

    Book initialized.

--- tutorial2/src/App.js
+++ tutorial2/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Book from "./Book";
 
 const App = () => {
   return (
     <section>
       <h1>This is yet another react app.</h1>
+      <Book />
     </section>
   );
 };
---
+++ tutorial2/src/Book.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Book extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is a book.</p>
+      </div>
+    );
+  }
+}
